fb7313c42a095937334bfb627212ce66bba1ede8
fusion <fusion@mail.com>
2023-10-07 19:19:31 +0300

    test setup

---
+++ NECS_Test/SerializationTest/SimpleObjectSerializationTest.cs
@@ -0,0 +1,21 @@
+﻿using NECS.ECS.DefaultsDB.ECSComponents;
+using NECS.ECS.ECSCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NECS_Test.SerializationTest
+{
+    [TestClass]
+    public class SimpleObjectSerializationTest
+    {
+        [TestMethod]
+        public void ObjectSerializationTest()
+        {
+            EntityManagersComponent component = new EntityManagersComponent();
+            BurstSerializationManager.DeepCopyByExpressionTree<EntityManagersComponent>(component);
+        }
+    }
+}
