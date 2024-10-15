176a6dfef6ddcd0587b804a349791ced7c63597d
fusion <fusion@mail.org>
2024-10-15 14:18:51 +0300

    fix

--- NECS/Extensions/Godot/GodotExtensions.cs
+++ NECS/Extensions/Godot/GodotExtensions.cs
@@ -22,34 +22,8 @@ public static class GodotExtensions
     {
         return new Vector3(X == null ? original.x : original.x + (float)X, Y == null ? original.y : original.y + (float)Y, Z == null ? original.z : original.z + (float)Z);
     }
-}
-#endif
-#if GODOT4_0_OR_GREATER
-using Godot;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public static class GodotExtensions
-{
-    public static Vector3 MoveTowardDistance(this Vector3 from, Vector3 to, float distance_delta)
-    {
-        return from.MoveToward(to, distance_delta * from.DistanceTo(to));
-    }
-
-    public static Vector3 Set(this Vector3 original, float? X = null, float? Y = null, float? Z = null)
-    {
-        return new Vector3( X == null ? original.X : (float)X, Y == null ? original.Y : (float)Y, Z == null ? original.Z : (float)Z);
-    }
-
-    public static Vector3 Increase(this Vector3 original, float? X = null, float? Y = null, float? Z = null)
-    {
-        return new Vector3(X == null ? original.X : original.X + (float)X, Y == null ? original.Y : original.Y + (float)Y, Z == null ? original.Z : original.Z + (float)Z);
-    }
-
-    public static void RecursiveCopy(string sourceFolder, string targetFolder)
+	
+	public static void RecursiveCopy(string sourceFolder, string targetFolder)
     {
         // Убедимся, что исходная папка существует
         if (!DirectoryExists(sourceFolder))
@@ -160,6 +134,32 @@ public static class GodotExtensions
         dest.Close();
         file.Dispose();
         dest.Dispose();
+    }
+}
+#endif
+#if GODOT4_0_OR_GREATER
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class GodotExtensions
+{
+    public static Vector3 MoveTowardDistance(this Vector3 from, Vector3 to, float distance_delta)
+    {
+        return from.MoveToward(to, distance_delta * from.DistanceTo(to));
+    }
+
+    public static Vector3 Set(this Vector3 original, float? X = null, float? Y = null, float? Z = null)
+    {
+        return new Vector3( X == null ? original.X : (float)X, Y == null ? original.Y : (float)Y, Z == null ? original.Z : (float)Z);
+    }
+
+    public static Vector3 Increase(this Vector3 original, float? X = null, float? Y = null, float? Z = null)
+    {
+        return new Vector3(X == null ? original.X : original.X + (float)X, Y == null ? original.Y : original.Y + (float)Y, Z == null ? original.Z : original.Z + (float)Z);
     }
 }
 #endif
